d1fadf5e7aba706abff884623bbb870128a7ccc6
willmp4 <w.goudie@outlook.com>
2024-05-15 08:24:57 +0100

    refactor: Update main.py to use messagebox for closing confirmation

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,16 +1,28 @@
 import tkinter as tk
 from gui import setup_gui
 from desktop_activity import ActivityMonitor
+from tkinter import messagebox
+
+def on_closing(activity_monitor, root):
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        activity_monitor.stop_monitoring()
+        root.destroy()
 
 def main():
-    root = tk.Tk()
-    root.title("Gaze Predictor App")
-    root.geometry("800x600")
 
+    root = tk.Tk()
+    root.title("Focus Monitoring App")
+
+    # Create an instance of ActivityMonitor
     activity_monitor = ActivityMonitor()
+
+    # Setup the GUI with the activity_monitor instance
     setup_gui(root, activity_monitor)
 
-    root.mainloop()
+    # Set the protocol for closing the window
+    root.protocol("WM_DELETE_WINDOW", lambda: on_closing(activity_monitor, root))
 
+    # Start the main event loop
+    root.mainloop()
 if __name__ == "__main__":
     main()
